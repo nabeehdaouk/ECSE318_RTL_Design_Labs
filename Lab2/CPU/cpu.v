116de8fffcf559bdc50dbd8dd21a18d9e8a9d051
nabeehdaouk <144377506+nabeehdaouk@users.noreply.github.com>
2023-10-05 22:29:57 -0400

    cpu updates and mem

--- Lab2/CPU/cpu.v
+++ Lab2/CPU/cpu.v
@@ -2,25 +2,32 @@ module cpu(
     input clk,
     input reset,
     input [31:0] data_in,
-    output [31:0] data_out,
-    output [11:0] address
+    output reg [31:0] data_out,
+    output reg [11:0] address,
+    output reg read,
+    output reg write,
+    output reg carry
 );
 
     reg [31:0] file_reg [15:0];
     reg [31:0] IR;
     reg [11:0] PC;
-    reg [2:0] PSR;
+    //reg [2:0] PSR;
     reg [2:0] IC; //instruction counter
 
     reg [31:0] opperand1;
     reg [31:0] opperand2;
+    reg [31:0] result;
+    reg [11:0] branch_address;
+    reg branch_valid;
+    
 
     localparam FETCH=     3'b000;
     localparam DECODE=    3'b001;
     localparam EXECUTE=   3'b010;
     localparam MEMORY=    3'b011;
     localparam WRITEBACK= 3'b101;
-    
+
     localparam NOP= 4'b0000;
     localparam LD=  4'b0001;
     localparam STR= 4'b0010;
@@ -31,7 +38,7 @@ module cpu(
     localparam SHF= 4'b0111;
     localparam HLT= 4'b1000;
     localparam CMP= 4'b1001;
-    
+
     localparam Always= 4'b0000;
     localparam Parity=  4'b0001;
     localparam Even= 4'b0010;
@@ -40,7 +47,8 @@ module cpu(
     localparam Zero= 4'b0101;
     localparam NoCarry= 4'b0110;
     localparam Posotive= 4'b0111;
-   
+    
+
 
 
 
@@ -51,7 +59,8 @@ module cpu(
             FETCH:
             begin
                 if (reset)
-                    begin IR<=0;
+                    begin
+                        IR<=0;
                     end
                 else
                     begin
@@ -67,20 +76,80 @@ module cpu(
 
             EXECUTE:
             begin
-                ADD:
-                result<= opperand1+opperand2;
+                case (IR[31:28]) //opcode
+                    NOP:
+                    begin
+                    result <= {32{1'b0}};
+                    carry <= 1'b0;
+                    branch_address <= {12{1'b0}};
+                    branch_valid <= 1'b0;
+                    read <= 1'b0;
+                    write<= 1'b0;
+                    end
+                    LD: 
+                    begin
+                    read <= 1'b1;
+                    write<= 1'b0;
+                    address <= execute[10:0];
+                    result <= {32{1'b0}};
+                    carry <= 1'b0;
+                    branch_address <= 0;
+                    branch_valid <= 0;
+                    end
+                    STR:
+                    begin
+                    end
+                    XOR:
+                    begin
+                        result<= opperand1 ^ opperand2;
+                        carry <= 1'b0;
+                        branch_address <= {12{1'b0}};
+                        branch_valid <= 1'b0;
+                        read_mem <= 1'b0;
+                    end
+                    ADD:
+                    begin
+                        {carry, result} <= opperand1 + opperand2;
+                        branch_address <= {12{1'b0}};
+                        branch_valid <= 1'b0;
+                        read_mem <= 1'b0;
+                    end
+                    ROT: 
+                    begin
+                    end
+                    SHF:
+                    begin
+                    end
+                    HLT: 
+                    begin
+                    end
+                    CMP:
+                    begin
+                    end
+                    BRA: 
+                    begin
+                    end
+                    default:
+                    begin
+                        //no op here
+                    end
+                    
+                    
+                    
+                endcase
+
             end
 
             MEMORY:
             begin
-                
+
             end
 
             WRITEBACK:
             begin
                 ADD:
                 file_reg[IR[15:12]<= result;
-                    //PC to mem
+                //PC to mem
                 ]
             end
 
