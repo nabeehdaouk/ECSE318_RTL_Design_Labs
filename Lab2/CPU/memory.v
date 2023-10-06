267bf14fd49ac4d113c5129a3dc01c9f43a74253
nabeehdaouk <144377506+nabeehdaouk@users.noreply.github.com>
2023-10-06 19:16:34 -0400

    clear mem function

--- Lab2/CPU/memory.v
+++ Lab2/CPU/memory.v
@@ -4,26 +4,39 @@ module memory(
     input mem_en,
     input read_write, //high indicates read in, low indicates write out
     input [31:0] data_in,
+    input clr_mem,
     output reg [31:0] data_out
 );
+    integer i;
     localparam write_out_mem= 1'b0;
     localparam read_in_mem= 1'b1;
     reg [31:0] mem [4095:0];
 
-    always @(posedge clk)
-    case(read_write)
-        write_out_mem:
-        begin
-            data_out <= mem_en ? mem[address]: {32{1'bZ}};
-        end
-        read_in_mem:
-        begin
-            mem[address] <= mem_en ? data_in: mem[address];
-        end
-        default:
-        begin //do nothing
-        end
-    endcase
 
+
+
+    always @(posedge clk)
+    begin
+        if (clr_mem == 1'b1)
+            begin
+                for (i=0; i<4096; i=i+1) begin
+                    mem[i] = 32'b0;
+                end
+            end else begin
+            case(read_write)
+                write_out_mem:
+                begin
+                    data_out <= mem_en ? mem[address]: {32{1'bZ}};
+                end
+                read_in_mem:
+                begin
+                    mem[address] <= mem_en ? data_in: mem[address];
+                end
+                default:
+                begin //do nothing
+                end
+            endcase
+        end
+    end
 
 endmodule
