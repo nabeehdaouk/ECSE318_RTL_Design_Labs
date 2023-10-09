997b38e583254f23035d2931d1cefd6642361515
nabeehdaouk <144377506+nabeehdaouk@users.noreply.github.com>
2023-10-08 21:29:45 -0400

    PROGRAM WORKING CCRAAAAANCHHHH

--- Lab2/CPU/test_prog_ones_tb.v
+++ Lab2/CPU/test_prog_ones_tb.v
@@ -55,7 +55,7 @@ module test_prog_ones_tb ();
         // PROGRAM MODE: 
     read_write = 1'b1;
     
-        data_in = 32'b00100100000000000011000000000000; //SNumberStored to count 
+        data_in = 32'hffff99f1; //SNumberStored to count 
         address = 12'h000;
         #20
 
@@ -75,30 +75,33 @@ module test_prog_ones_tb ();
         address = 12'h006;
         #20
 
-        data_in = 32'b0011_0101_000000001011_000000000000; //BRANCH if ZERO to adrs 11
+        data_in = 32'b0011_0110_000000001011_000000000000; //BRANCH if NO CARRY to adrs 11
         address = 12'h007;
         #20
 
-        data_in = 32'b0011_0110_000000001010_000000000000; //BRANCH if NO CARRY to adrs 10
+        data_in = 32'b0101_1_0_00_000000000001_000000000010; //ADD #1 REG2, result in REG2
         address = 12'h008;
         #20
 
-
-        data_in = 32'b0101_1_0_00_000000000001_000000000010; //ADD #1 REG2, result in REG2
-        read_write = 1'b1;
+        data_in = 32'b0101_1_0_00_000000000000_000000000000; //ADD #0 REG0, result in REG0, PSR refclects REG0
         address = 12'h009;
         #20
 
-        data_in = 32'b0011_0000_000000000110_000000000110; //BRANCH ALWAYS to adrs 5
+
+        data_in = 32'b0011_0101_000000001100_000000000000; //BRANCH if ZERO to adrs 12
         address = 12'h00a;
         #20
 
-        data_in = 32'b0010_0_1_00_000000000010_000000000001; //STR REG2 MEM1
+        data_in = 32'b0011_0000_000000000110_000000000000; //BRANCH ALWAYS to adrs 6
         address = 12'h00b;
         #20
 
-        data_in = 32'b1001_0_0_00_000000000000_000000000000; //HLT program
+        data_in = 32'b0010_0_1_00_000000000010_000000000001; //STR REG2 MEM1
         address = 12'h00c;
+        #20
+
+        data_in = 32'b1000_0_0_00_000000000000_000000000000; //HLT program
+        address = 12'h00d;
         #20
 //-----------------DATA_STORRED--------------------------------------------------
 
