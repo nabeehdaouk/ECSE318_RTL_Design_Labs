6782162519b1cf61c079fd4ad81147361162e849
kyleheston <144495240+kyleheston@users.noreply.github.com>
2023-10-08 22:34:31 -0400

    cputop comment

--- Lab2/CPU/cpu_top.v
+++ Lab2/CPU/cpu_top.v
@@ -1,40 +1,39 @@
 module cpu_top (
-    input reset,
-    input cpu_en,
-    input main_clk,
-    input clr_mem,
-    input read_write,
-    input mem_en,
-    input [11:0] address,
-    input [31:0] data_in,
-    output [31:0] read_out_data
+    input main_clk, 
+    input reset, //For CPU programming
+    input cpu_en, //For CPU programming
+    input clr_mem, //For CPU programming
+    input read_write, //For CPU programming
+    input mem_en, //For CPU programming
+    input [11:0] address, //For CPU programming
+    input [31:0] data_in, //For CPU programming
+    output [31:0] read_out_data //For CPU test validation
 );
 
-    wire [31:0] data_to_cpu;
-    wire [31:0] data_to_mem;
-    wire [11:0] address_cpu_mem;
-    wire clr_mem_cpu;
-    wire mem_en_cpu;
-    wire read_write_cpu;
-
-    wire gated_clk;
-    wire [11:0] gated_address;
-    wire [31:0] gated_data_to_mem;
-    wire gated_clr_mem;
-    wire gated_read_write;
-    wire gated_mem_en;
+    wire [31:0] data_to_cpu; //For CPU operation with memory, intercepted for CPU validation
+    wire [31:0] data_to_mem; //For CPU operation with memory
+    wire [11:0] address_cpu_mem; //For CPU operation with memory
+    wire clr_mem_cpu; //For CPU operation with memory
+    wire mem_en_cpu; //For CPU operation with memory
+    wire read_write_cpu; //For CPU operation with memory
+    wire gated_clk; //For CPU operation with memory
+    wire [11:0] gated_address; //For CPU operation with memory
+    wire [31:0] gated_data_to_mem; //For CPU operation with memory
+    wire gated_clr_mem; //For CPU operation with memory
+    wire gated_read_write; //For CPU operation with memory
+    wire gated_mem_en; //For CPU operation with memory
    
 
 
 
 
-    assign gated_clk= (cpu_en)? main_clk: 1'b0;
+    assign gated_clk= (cpu_en)? main_clk: 1'b0; 
     assign gated_address= (cpu_en)? address_cpu_mem: address;
     assign gated_data_to_mem= (cpu_en)? data_to_mem: data_in;
-    assign gated_clr_mem= (cpu_en)? clr_mem_cpu: clr_mem;
-    assign gated_read_write= (cpu_en)? read_write_cpu: read_write;
+    assign gated_clr_mem= (cpu_en)? clr_mem_cpu: clr_mem; 
+    assign gated_read_write= (cpu_en)? read_write_cpu: read_write; 
     assign gated_mem_en= (cpu_en)? mem_en_cpu: mem_en;
-    assign read_out_data= data_to_cpu;
+    assign read_out_data= data_to_cpu; 
 
     memory memory_instance(
         .clk(main_clk),
