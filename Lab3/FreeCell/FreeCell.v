6b304e0c30a59fa07c1f7e10d929a44437eaa1c8
nabeehdaouk <144377506+nabeehdaouk@users.noreply.github.com>
2023-10-27 17:31:31 -0400

    Update FreeCell.v

--- Lab3/FreeCell/FreeCell.v
+++ Lab3/FreeCell/FreeCell.v
@@ -22,52 +22,120 @@ module FreeCell(
     localparam queen = 4'b1100;
     localparam king  = 4'b1101;
 
-    reg [5:0] tableau[7:0][6:0];
-    reg [3:0] home_cells[3:0]; // Array to store home cells
+    reg [5:0] tableau[7:0][21:0];
+    reg [3:0] home_cells[3:0]; // Array to store home cells used as COUNTRER
+    reg [5:0] free_cells[3:0]; // Array to store free cell card
     reg [3:0] source_column; // Extracted source column
     reg [3:0] dest_column; // Extracted destination column
-    
+
     always @(posedge clk) begin
-        // Process moves at each clock cycle
-        // Extract source and destination columns
+        case({source, dest})
+        {4'b0???, 4'b0???}: //col to col
+        begin
+            if ((tableau[dest[2:0]][0]== (tableau[source[2:0]][0]) +1'b1) & )
+            begin
+            tableau[dest[2:0]][0]<= tableau[source[2:0]][0];
+            tableau[dest[2:0]][21:1] <= tableau[dest[2:0]][20:0];
+            tableau[source[2:0]][20:0] <= tableau[source[2:0]][21:1];
+            end else begin
+        end
+        end
+        
+
+        {4'b0???, 4'b10??}: //col to free_cell
+begin
+            free_cells[dest[1:0]]<= tableau[source[2:0]][0];
+            tableau[source[2:0]][20:0] <= tableau[source[2:0]][21:1];
+        end
+        
+        {4'b0???, 4'b11??}: //col to home_cell
+        begin
+            //home_cells
+            tableau[source[2:0]][20:0] <= tableau[source[2:0]][21:1];
+        end
+        
+        {4'b10??, 4'b0???}: //free_cell to col
+        begin
+           tableau[dest[2:0]][0] <= free_cells[source[1:0]]; 
+           tableau[dest[2:0]][21:1] <= tableau[dest[2:0]][20:0];
+           free_cells[source[1:0]]<= 5'b00000;
+        end
+
+        {4'b10??, 4'b10??}: //free_cell to free_cell
+        begin
+           free_cells[dest[1:0]] <= free_cells[source[1:0]]; 
+           free_cells[source[1:0]]<= 5'b00000;
+        end
+        
+        {4'b10??, 4'b11??}: //free_cell to home_cell
+        begin
+            free_cells[source[1:0]]<= 5'b00000;
+        end
+        
+        default: //illigal
+        begin
+        end
+
+        endcase
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         if (source[3] == 0) begin
-            source_column <= source[2:0];
+        tableau[]
+        source_column <= source[2:0];
         end else if (source[3:2] == 2'b10) begin
-            // Handle free cell source
-            // You need to add logic for this
-            // For simplicity, you can assign source_column to a constant value
-            source_column <= 3'b000;
+        // Handle free cell source
+        // You need to add logic for this
+        // For simplicity, you can assign source_column to a constant value
+        source_column <= 3'b000;
         end else begin
-            // Illegal source
-            // Mark the move as illegal
-            source_column <= 3'b111; // Mark it as an invalid column
+        // Illegal source
+        // Mark the move as illegal
+        source_column <= 3'b111; // Mark it as an invalid column
         end
 
         if (dest[3] == 0) begin
-            dest_column <= dest[2:0];
+        dest_column <= dest[2:0];
         end else if (dest[3:2] == 2'b10) begin
-            // Handle free cell destination
-            // You need to add logic for this
-            // For simplicity, you can assign dest_column to a constant value
-            dest_column <= 3'b000;
+        // Handle free cell destination
+        // You need to add logic for this
+        // For simplicity, you can assign dest_column to a constant value
+        dest_column <= 3'b000;
         end else begin
-            // Home cell destination
-            // Determine the correct home cell based on the source
-            case (source[3:2])
-                2'b00: dest_column <= home_cells[0];
-                2'b01: dest_column <= home_cells[1];
-                2'b10: dest_column <= home_cells[2];
-                2'b11: dest_column <= home_cells[3];
-                default: dest_column = 3'b111; // Mark it as an invalid column
-            endcase
+        // Home cell destination
+        // Determine the correct home cell based on the source
+        case (source[3:2])
+        2'b00: dest_column <= home_cells[0];
+        2'b01: dest_column <= home_cells[1];
+        2'b10: dest_column <= home_cells[2];
+        2'b11: dest_column <= home_cells[3];
+        default: dest_column = 3'b111; // Mark it as an invalid column
+        endcase
         end
 
         // Move processing logic
         if (source_column != 3'b111 && dest_column != 3'b111) begin
-            // Check if the move is legal and process it
-            // You need to add logic for this based on the rules of FreeCell
-            // For simplicity, you can update tableau and home_cells based on source and destination
-            // and check if the move is a winning move
+        // Check if the move is legal and process it
+        // You need to add logic for this based on the rules of FreeCell
+        // For simplicity, you can update tableau and home_cells based on source and destination
+        // and check if the move is a winning move
         end
 
         // Check if the game is won (you need to add this logic)
@@ -75,77 +143,77 @@ module FreeCell(
     end
 
     initial begin
-        // Col 1
-        tableau[0][0] = {s, four};
-        tableau[0][1] = {d, jack};
-        tableau[0][2] = {d, ten};
-        tableau[0][3] = {d, six};
-        tableau[0][4] = {s, three};
-        tableau[0][5] = {d, ace};
-        tableau[0][6] = {h, ace};
+    // Col 1
+    tableau[0][0] = {s, four};
+    tableau[0][1] = {d, jack};
+    tableau[0][2] = {d, ten};
+    tableau[0][3] = {d, six};
+    tableau[0][4] = {s, three};
+    tableau[0][5] = {d, ace};
+    tableau[0][6] = {h, ace};
 
-        // Col 2
-        tableau[1][0] = {s, five};
-        tableau[1][1] = {s, ten};
-        tableau[1][2] = {h, eight};
-        tableau[1][3] = {c, four};
-        tableau[1][4] = {h, six};
-        tableau[1][5] = {h, king};
-        tableau[1][6] = {h, two};
+    // Col 2
+    tableau[1][0] = {s, five};
+    tableau[1][1] = {s, ten};
+    tableau[1][2] = {h, eight};
+    tableau[1][3] = {c, four};
+    tableau[1][4] = {h, six};
+    tableau[1][5] = {h, king};
+    tableau[1][6] = {h, two};
 
-        // Col 3
-        tableau[2][0] = {s, jack};
-        tableau[2][1] = {c, seven};
-        tableau[2][2] = {c, nine};
-        tableau[2][3] = {c, six};
-        tableau[2][4] = {c, two};
-        tableau[2][5] = {s, king};
-        tableau[2][6] = {c, ace};
+    // Col 3
+    tableau[2][0] = {s, jack};
+    tableau[2][1] = {c, seven};
+    tableau[2][2] = {c, nine};
+    tableau[2][3] = {c, six};
+    tableau[2][4] = {c, two};
+    tableau[2][5] = {s, king};
+    tableau[2][6] = {c, ace};
 
-        // Col 4
-        tableau[3][0] = {h, four};
-        tableau[3][1] = {s, ace};
-        tableau[3][2] = {c, queen};
-        tableau[3][3] = {c, five};
-        tableau[3][4] = {s, seven};
-        tableau[3][5] = {h, nine};
-        tableau[3][6] = {s, eight};
+    // Col 4
+    tableau[3][0] = {h, four};
+    tableau[3][1] = {s, ace};
+    tableau[3][2] = {c, queen};
+    tableau[3][3] = {c, five};
+    tableau[3][4] = {s, seven};
+    tableau[3][5] = {h, nine};
+    tableau[3][6] = {s, eight};
 
-        // Col 5
-        tableau[4][0] = {d, queen};
-        tableau[4][1] = {h, jack};
-        tableau[4][2] = {s, queen};
-        tableau[4][3] = {s, six};
-        tableau[4][4] = {d, two};
-        tableau[4][5] = {s, nine};
-        //tableau[4][6] = {};
+    // Col 5
+    tableau[4][0] = {d, queen};
+    tableau[4][1] = {h, jack};
+    tableau[4][2] = {s, queen};
+    tableau[4][3] = {s, six};
+    tableau[4][4] = {d, two};
+    tableau[4][5] = {s, nine};
+    //tableau[4][6] = {};
 
-        // Col 6
-        tableau[4][0] = {d, five};
-        tableau[4][1] = {d, king};
-        tableau[4][2] = {c, three};
-        tableau[4][3] = {d, nine};
-        tableau[4][4] = {h, three};
-        tableau[4][5] = {s, two};
-        //tableau[4][6] = {}; 
+    // Col 6
+    tableau[4][0] = {d, five};
+    tableau[4][1] = {d, king};
+    tableau[4][2] = {c, three};
+    tableau[4][3] = {d, nine};
+    tableau[4][4] = {h, three};
+    tableau[4][5] = {s, two};
+    //tableau[4][6] = {}; 
 
-        // Col 7
-        tableau[6][0] = {h, five};
-        tableau[6][1] = {d, three};
-        tableau[6][2] = {h, queen};
-        tableau[6][3] = {d, seven};
-        tableau[6][4] = {c, king};
-        tableau[6][5] = {c, ten};
-        //tableau[6][6] = {};
+    // Col 7
+    tableau[6][0] = {h, five};
+    tableau[6][1] = {d, three};
+    tableau[6][2] = {h, queen};
+    tableau[6][3] = {d, seven};
+    tableau[6][4] = {c, king};
+    tableau[6][5] = {c, ten};
+    //tableau[6][6] = {};
 
-        // Col 8
-        tableau[7][0] = {c, jack};
-        tableau[7][1] = {d, four};
-        tableau[7][2] = {h, ten};
-        tableau[7][3] = {c, eight};
-        tableau[7][4] = {h, seven};
-        tableau[7][5] = {d, eight};
-        // tableau[7][6] = {};   
+    // Col 8
+    tableau[7][0] = {c, jack};
+    tableau[7][1] = {d, four};
+    tableau[7][2] = {h, ten};
+    tableau[7][3] = {c, eight};
+    tableau[7][4] = {h, seven};
+    tableau[7][5] = {d, eight};
+    // tableau[7][6] = {};   
 
 
     end
