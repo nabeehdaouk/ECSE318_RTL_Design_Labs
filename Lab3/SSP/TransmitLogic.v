0f13ec2fbced6328fe39dd0d0fc6f85c4e39ee1a
nabeehdaouk <144377506+nabeehdaouk@users.noreply.github.com>
2023-10-29 22:00:17 -0400

    fix it

---
+++ Lab3/SSP/TransmitLogic.v
@@ -0,0 +1,85 @@
+module TransmitLogic(
+    input [7:0] TxData,
+    input pclk, clr_b, flag_empty, flag_full,
+    output ssp_fss_out,
+    output reg ssp_oe_b, ssp_txd, ssp_clk_out, inc_ptr
+);
+
+    reg [7:0] data;
+    reg data_read;
+    reg [3:0] counter;
+
+    initial begin
+        ssp_clk_out= 1'b0;
+        inc_ptr= 1'b0;
+        counter = 4'b0000;
+        ssp_oe_b =1'b1;
+    end
+
+    always @(posedge pclk)
+    begin
+        ssp_clk_out <= ~ssp_clk_out;
+    end
+
+    assign ssp_fss_out = inc_ptr;
+
+    always @(posedge ssp_clk_out, clr_b)
+    begin
+        if (clr_b ==1'b1)
+            begin
+                data <= 8'b0;
+                counter <= 4'b0000;
+                data_read<= 1'b0;
+                inc_ptr<= 1'b0;
+            end
+        else begin
+            if (inc_ptr ==1'b1)
+                begin
+                    inc_ptr<= 1'b0;
+                    data_read <= 1'b0;
+                    counter <= 4'b0000;
+                end
+            else if (inc_ptr == 1'b0)
+            begin
+                if ((data_read == 1'b0) && (flag_empty== 1'b0)) //empty chec
+                    begin
+                        data <= TxData;
+                        data_read <= 1'b1;
+                    end
+                else
+                    begin
+                        //do nothing
+                    end
+            end
+
+        end
+    end
+
+    always @(posedge ssp_clk_out)
+    begin
+        if ((data_read ==1'b1) && (counter<4'b1000))
+        begin
+            ssp_txd <= data[7];
+            data <= data<<1;
+            counter<= counter +1'b1;
+        end
+        if ((counter == 4'b1000) && (flag_empty == 1'b0))
+        begin
+            inc_ptr<= 1'b1;
+        end
+    end
+
+
+    always @(negedge ssp_clk_out)
+    if (ssp_fss_out == 1'b1)
+        begin
+            ssp_oe_b <= 1'b0;
+        end
+    else
+        begin
+        ssp_oe_b<= (counter == 4'b1000)? 1'b1: 1'b0;
+        end
+
+
+
+endmodule
---
+++ Lab3/SSP/TransmitLogic_tb.v
@@ -0,0 +1,52 @@
+module TransmitLogic_tb();
+	
+	reg [7:0] TxData;
+	reg pclk, clr_b, flag_empty, flag_full;
+	wire ssp_fss_out, ssp_oe_b, ssp_txd, ssp_clk_out, inc_ptr;
+	
+	
+	
+	TransmitLogic TransmitLogic_instance(
+	    .TxData(TxData),
+	    .pclk(pclk),
+	    .clr_b(clr_b),
+	    .flag_empty(flag_empty),
+	    .flag_full(flag_full),
+	    .ssp_fss_out(ssp_fss_out),
+	    .ssp_oe_b(ssp_oe_b),
+	    .ssp_txd(ssp_txd),
+	    .ssp_clk_out(ssp_clk_out),
+	    .inc_ptr(inc_ptr)
+	);
+	
+	initial begin
+	pclk = 0;
+	clr_b= 1; 
+	#20
+	clr_b=0;
+	
+	TxData= 8'b11001010;
+	flag_empty= 0;
+	flag_full= 0;
+	
+	#60
+$display("EMOPTY FLAH");
+	flag_empty= 1;
+	#300
+$display("EMOPTY FLAH GONE");
+    TxData= 8'b10101011;
+    flag_empty= 0;
+    #300
+	
+    
+    
+$stop;
+	
+end
+
+always #5 pclk<= ~pclk;
+
+always #20 $display( ssp_txd, "          ",ssp_fss_out );
+	
+	
+endmodule
